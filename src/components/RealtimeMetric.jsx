6dabe02376860db11ee17713ed717f7da5d6c941
Sailaja <sailajabevara2006@gmail.com>
2026-02-12 14:24:08 +0530

    Step 6 completed

--- src/components/RealtimeMetric.jsx
+++ src/components/RealtimeMetric.jsx
@@ -1,27 +1,4 @@
-// import { useContext } from "react";
-// import { WebSocketContext } from "../contexts/WebSocketContext";
 
-// function RealtimeMetric() {
-
-//   const { metric, status } = useContext(WebSocketContext);
-
-//   return (
-//     <div style={{
-//       padding: "20px",
-//       border: "2px solid #ddd",
-//       borderRadius: "10px",
-//       width: "300px"
-//     }}>
-//       <h2>Active Users</h2>
-
-//       <h1>{metric}</h1>
-
-//       <p>Status: {status}</p>
-//     </div>
-//   );
-// }
-
-// export default RealtimeMetric;
 
 import { useContext } from "react";
 import { WebSocketContext } from "../contexts/WebSocketContext";
@@ -38,15 +15,40 @@ const RealtimeMetric = () => {
 
   return (
     <div>
+
       <h2>Active Users</h2>
 
+      {/*Show warning when disconnected */}
+      {status === "Disconnected" && (
+        <div
+          style={{
+            background: "#ffe5e5",
+            padding: "10px",
+            borderRadius: "8px",
+            marginBottom: "10px",
+            color: "red",
+            fontWeight: "bold"
+          }}
+        >
+           Connection lost. Trying to reconnect...
+        </div>
+      )}
+
+      {/*  Loading state */}
       <h1 style={{ fontSize: "48px", margin: "10px 0" }}>
-        {metric === 0 ? "Loading..." : metric}
+        {metric === 0 ? "Loading realtime data..." : metric}
       </h1>
 
-      <p style={{ color: getStatusColor(), fontWeight: "bold" }}>
+      {/* Status with color */}
+      <p
+        style={{
+          fontWeight: "bold",
+          color: getStatusColor(),
+        }}
+      >
         Status: {status}
       </p>
+
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { WebSocketProvider } from "./contexts/WebSocketContext";
+import ErrorBoundary from "./components/ErrorBoundary";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+    <ErrorBoundary> 
     <WebSocketProvider>
       <App />
     </WebSocketProvider>
+    </ErrorBoundary>
   
 )
